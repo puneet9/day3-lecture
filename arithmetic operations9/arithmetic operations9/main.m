7ff9db363e66bc8338cd068b8ae115aaeee86f3c
puneet9 <user1@Administrators-iMac-75.local>
2014-05-07 14:49:41 -0400

    ee

---
+++ arithmetic operations9/arithmetic operations9/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  arithmetic operations9
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 puneet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        int a =100;
+        int b = 2;
+        int c = 25;
+        int d = 4;
+        int result;
+        result = a-b;
+    }
+    return 0;
+}
+
